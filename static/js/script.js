e1a38ed7c7af1d0ff0e4fa918927089059893aaa
gpa21 <gpa21@sfu.ca>
2024-10-06 00:15:55 -0400

    update

--- static/js/script.js
+++ static/js/script.js
@@ -11,25 +11,19 @@ fetch('/static/dataset/GLAD_Confidence_Footprint.geojson')
       style: function (feature) {
         return {
           color: '#ff0000',
-          weight: 1, // Outline the polygons
+          weight: 0,
           fillColor: '#ff9999',
-          fillOpacity: 0 // Make fill transparent
+          fillOpacity: 0 
         };
       },
       onEachFeature: function (feature, layer) {
         if (feature.properties) {
           layer.bindPopup(`<b>${feature.properties.NAME}</b>`); 
         }
-
-        // Set the seed based on the polygon's NAME or a default value
         var seed = feature.properties.NAME || 'default-seed';
-        // Initialize the seeded random number generator
         var rng = new Math.seedrandom(seed);
+        var randomPoints = generateRandomPointsInPolygon(feature, 20, rng);
 
-        // Generate random points within the polygon
-        var randomPoints = generateRandomPointsInPolygon(feature, 20, rng); // Adjust the number of points as needed
-
-        // Add circle markers for each random point
         randomPoints.forEach(function(coords) {
           L.circleMarker([coords[1], coords[0]], {
             radius: 3,
@@ -43,7 +37,6 @@ fetch('/static/dataset/GLAD_Confidence_Footprint.geojson')
   })
   .catch(error => console.error('Error loading the GeoJSON:', error));
 
-// Function to generate random points within a polygon using a seeded RNG
 function generateRandomPointsInPolygon(polygon, numPoints, rng) {
   var points = [];
   var bbox = turf.bbox(polygon);
